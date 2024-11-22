9f9bfc47c76b9d0c303338a931b0dda1d451df0d
nomanr <n@nomanr.com>
2024-11-22 22:35:46 +0000

    generate initial template

--- compose-ui/scripts/generate_templates.sh
+++ compose-ui/scripts/generate_templates.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-SOURCE_DIR="components-lab/src/main/java/com/nomanr/compose/ui"
-DEST_DIR="plugin/src/main/java/resources/templates"
+SOURCE_DIR="../components-lab/src/main/java/com/nomanr/compose/ui"
+DEST_DIR="../plugin/src/main/java/resources/templates"
+
+check_directory_exists() {
+  local dir="$1"
+  if [ ! -d "$dir" ]; then
+    echo "Error: Directory not found -> $dir"
+    exit 1
+  fi
+}
 
 process_files() {
   local src="$1"
@@ -14,13 +22,21 @@ process_files() {
       process_files "$item" "$dest/$(basename "$item")"
     elif [ -f "$item" ]; then
       local filename=$(basename "$item")
-      local dest_file="$dest/$filename"
+      local dest_file="$dest/${filename%.kt}.kt.template"
 
-      sed 's/\bpackage\b/package {{PACKAGE_NAME}}/' "$item" > "$dest_file"
+      if ! sed -e "s/^package com\.nomanr\.compose\.ui\.\(.*\)/package {{packageName}}.\1/" \
+               -e 's/import com\.nomanr\.compose\.ui\.\(.*\)/import {{packageName}}.\1/' \
+               "$item" > "$dest_file"; then
+        echo "Error: Failed to process file -> $item"
+        exit 1
+      fi
+    else
+      echo "Warning: Skipping unrecognized item -> $item"
     fi
   done
 }
 
+echo "Running script from: $(pwd)"
+check_directory_exists "$SOURCE_DIR"
 process_files "$SOURCE_DIR" "$DEST_DIR"
-
-echo "Template files generated in $DEST_DIR"
+echo "Template files successfully generated in $DEST_DIR"
