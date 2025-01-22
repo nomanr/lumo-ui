e2675d541b53e3c945e471b32109e3a8462b6627
Scott <schott12521@gmail.com>
2025-01-22 13:34:41 -0800

    feat: adding multiplatform components lab, attempting to fix them for KMP

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,6 +24,7 @@ dependencyResolutionManagement {
 
 rootProject.name = "lumo-ui"
 include(":lumo-ui:components-lab")
+include(":lumo-ui:multiplatform-components-lab")
 include(":sample-android:catalogue")
 include(":sample-android:ui-components")
 include(":sample-multiplatform:catalogue:common")
