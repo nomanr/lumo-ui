3e5a819c5d71afb57aefe4e8dee5a0a8959c5850
Scott <schott12521@gmail.com>
2025-01-23 20:06:06 -0800

    fix: okay trying to clean things up a bit

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,22 +4,17 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-
     }
 }
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.spotless) apply false
     alias(libs.plugins.vanniktech.maven.publish) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.compose.multiplatform) apply false
-
 }
 
 subprojects {
@@ -40,10 +35,7 @@ subprojects {
                     "android" to "true"
                 )
             )
-
-
         }
     }
-
 }
 
