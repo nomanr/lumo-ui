071bda9b1ea55c2ffedaa41e2b5b6e0516a48ff3
nomanr <nomrafique@gmail.com>
2025-02-27 21:01:16 +0000

    fix: add device frame param

--- sample-multiplatform/catalogue/web/src/wasmJsMain/kotlin/com/nomanr/lumo/multiplatform/sample/Main.kt
+++ sample-multiplatform/catalogue/web/src/wasmJsMain/kotlin/com/nomanr/lumo/multiplatform/sample/Main.kt
@@ -4,9 +4,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -16,16 +16,37 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeViewport
 import com.nomanr.lumo.multiplatform.ui.AppTheme
 import kotlinx.browser.document
+import kotlinx.browser.window
+import org.w3c.dom.url.URLSearchParams
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport(document.body!!) {
-        Box(modifier = Modifier.fillMaxSize().background(AppTheme.colors.background), contentAlignment = Alignment.Center) {
-            Box(
-                modifier = Modifier.size(size = DpSize(400.dp, 800.dp))
-                    .border(5.dp, color = AppTheme.colors.primary, shape = RoundedCornerShape(12.dp))
-                    .clip(RoundedCornerShape(12.dp))
-            ) { CatalogueApp() }
+        val mobileMode = mobileMode()
+        if (mobileMode) {
+            MobileFrame {
+                CatalogueApp()
+            }
+        } else {
+            CatalogueApp()
         }
     }
+}
+
+
+@Composable
+fun MobileFrame(content: @Composable () -> Unit) {
+    Box(modifier = Modifier.fillMaxSize().background(AppTheme.colors.background), contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier.size(size = DpSize(400.dp, 800.dp))
+                .border(5.dp, color = AppTheme.colors.primary, shape = RoundedCornerShape(12.dp))
+                .clip(RoundedCornerShape(12.dp)),
+        ) { content() }
+    }
+}
+
+
+fun mobileMode(): Boolean {
+    val searchParams = URLSearchParams(window.location.search.toJsString())
+    return searchParams.has("mobileMode")
 }
