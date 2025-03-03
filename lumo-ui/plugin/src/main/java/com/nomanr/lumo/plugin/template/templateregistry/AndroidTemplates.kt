c6f7195458ae34f2968fab298eac71e6f644fa2b
nomanr <nomrafique@gmail.com>
2025-03-03 16:22:12 +0000

    android cleanup

--- lumo-ui/plugin/src/main/java/com/nomanr/lumo/plugin/template/templateregistry/AndroidTemplates.kt
+++ lumo-ui/plugin/src/main/java/com/nomanr/lumo/plugin/template/templateregistry/AndroidTemplates.kt
@@ -1,21 +1,11 @@
 package com.nomanr.lumo.plugin.template.templateregistry
 
-import com.nomanr.lumo.plugin.template.templateregistry.SupportedComponents.ModalBottomSheet
 import com.nomanr.lumo.plugin.template.templateregistry.SupportedComponents.Slider
 import com.nomanr.lumo.plugin.template.templateregistry.SupportedComponents.Surface
 import com.nomanr.lumo.plugin.template.templateregistry.SupportedComponents.SystemBars
 import com.nomanr.lumo.plugin.template.templateregistry.SupportedComponents.Tooltip
 
 object AndroidTemplates {
-    private val modalBottomSheet =
-        Template(
-            componentFiles = listOf("components/ModalBottomSheet.kt.template"),
-            requirements =
-                "Note: Add the following dependency to the project to use this component: \n" +
-                    "implementation(\"com.nomanr:composables:{version}\")\n" +
-                    "Reference: https://github.com/nomanr/compose-components",
-        )
-
     private val slider =
         Template(
             componentFiles = listOf("components/Slider.kt.template"),
@@ -34,7 +24,6 @@ object AndroidTemplates {
 
     fun getTemplates() =
         mapOf(
-            ModalBottomSheet to modalBottomSheet,
             Slider to slider,
             SystemBars to systemBars,
             Tooltip to toolTip,
